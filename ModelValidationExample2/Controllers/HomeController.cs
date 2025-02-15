a8da7420090babb4a405a668aef2325d400b0196
hoangnhancs <thaihoangnhantk17lqd@gmail.com>
2025-02-15 13:47:17 +0700

    Add project files.

---
+++ ModelValidationExample2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample2.Models;
+
+namespace ModelValidationExample2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                //get error messages from model state
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(errors);
+            }
+
+            return Content($"{person}");
+        }
+    }
+}
